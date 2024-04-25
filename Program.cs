8b77e03ed6179752488ac079a29a77fedee1ae83
Mazin Mohammed <21mmohammed@heckgrammar.co.uk>
2024-04-26 00:08:08 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -10,9 +10,59 @@ namespace CowsAndBulls
     {
         static void Main(string[] args)
         {
-            //Cows and Bulls Starter Framework Code
+            {
+                Console.WriteLine("guess the 4 digit number");
+                Random rand = new Random();
+                HashSet<int> genNumbers = new HashSet<int>();
+                int[] RanNumber = new int[4];
+                for (int i = 0; i < RanNumber.Length; i++)
+                {
+                    int randomNumber;
+                    do
+                    {
+                        randomNumber = rand.Next(0, 10);
+                    }
+                    while (genNumbers.Contains(randomNumber));
 
-            Console.ReadLine();
+                    RanNumber[i] = randomNumber;
+                    genNumbers.Add(randomNumber);
+                }
+                int attempts = 0;
+                while (true)
+                {
+                    attempts++;
+                    Console.WriteLine($"Attempt #{attempts}:");
+                    string input = Console.ReadLine();
+                    if (input.Length != 4 || !int.TryParse(input, out _))
+                    {
+                        Console.WriteLine("number is not 4 digits");
+                        continue;
+                    }
+                    int[] guess = new int[4];
+                    for (int i = 0; i < guess.Length; i++)
+                    {
+                        guess[i] = Convert.ToInt32(input[i].ToString());
+                    }
+                    int cows = 0;
+                    int bull = 0;
+                    for (int i = 0; i < guess.Length; i++)
+                    {
+                        if (guess[i] == RanNumber[i])
+                        {
+                            bull++;
+                        }
+                        else if (genNumbers.Contains(guess[i]))
+                        {
+                            cows++;
+                        }
+                    }
+                    Console.WriteLine($"Cows: {cows}, Bulls: {bull}");
+                    if (bull == 4)
+                    {
+                        Console.WriteLine("that is correct");
+                    }
+                }
+            }
         }
     }
 }
